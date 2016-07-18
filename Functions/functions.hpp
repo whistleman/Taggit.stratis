2fef36eb177f5104c37301360389fa8e55b236bc
WhistleTDNL <jacco.h@live.nl>
2016-07-18 19:02:08 +0200

    init mission.sqm + fixed functions.hpp

--- Functions/functions.hpp
+++ Functions/functions.hpp
@@ -2,9 +2,8 @@ class WIS
 {
 	class Taggit 
 	{
-	file = "functions";
-	class Handle_score {};
-	class ScoreLoop {};
-	class Switch {};
+		class Handle_score {file = "functions\fn_handlescore.sqf";};
+		class ScoreLoop {file = "functions\fn_Scoreloop.sqf";};
+		class Switch {file = "functions\fn_Switch.sqf";};
 	};
 };
