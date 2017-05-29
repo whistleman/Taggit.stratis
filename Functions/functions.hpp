a20558179b2d3af2c23d69f3b46e286ec2a7a928
WhistleTDNL <jacco.h@live.nl>
2017-05-29 17:53:38 +0200

    functions not correctly put in...

--- Functions/functions.hpp
+++ Functions/functions.hpp
@@ -3,28 +3,28 @@ class WIS
 	class Main
 	{
 		file = "functions\main";
-		class Handle_score {file = "fn_handle_score.sqf";};
-		class ScoreLoop {file = "fn_Scoreloop.sqf";};
-		class Switch {file = "fn_Switch.sqf";};
-		class showHint {file = "fn_showHint.sqf";};
-		class SelectStartingPoint {file = "fn_selectStartingPoint.sqf";};
-		class setState {file = "fn_setState.sqf";};
-		class CreatePubVarEH {file = "fn_CreatePubVarEH.sqf";};
-		class resetAmmo {file = "fn_resetAmmo.sqf";};
+		class Handle_score {};
+		class ScoreLoop {};
+		class Switch {};
+		class showHint {};
+		class SelectStartingPoint {};
+		class setState {};
+		class CreatePubVarEH {};
+		class resetAmmo {};
 	};
 	class Abilities
 	{
 		file = "functions\abilities";
-		class Ability_CantMove {file = "functions\fn_Ability_CantMove.sqf";};
-		class Ability_ShowTagged {file = "functions\fn_Ability_ShowTagged.sqf";};
-		class Ability_Blackout {file = "functions\fn_Ability_Blackout.sqf";};
-		class AddActions {file = "functions\fn_AddActions.sqf";};
-		class HandleActions {file = "functions\fn_HandleActions.sqf";};
+		class Ability_CantMove {};
+		class Ability_ShowTagged {};
+		class Ability_Blackout {};
+		class AddActions {};
+		class HandleActions {};
 	};
 	class Debug
 	{
 		file = "functions\debug";
-		class Debug {file="functions\fn_Debug.sqf";};
-		class DebugServer {file="functions\fn_DebugServer.sqf";};
+		class Debug {};
+		class DebugServer {};
 	};
 };
